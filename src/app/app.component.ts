918a2883637a84c610ff0d93fa6d6cc09fe55f64
Alejandro Avilez <aleantonio656@gmail.com>
2022-12-11 16:34:20 -0600

    cards de NgPrime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string = 'AlEjANdro AViLez';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Alejandro'
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre)
+    console.log(this.valor)
+    console.log(this.obj);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+//Prime NG
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
